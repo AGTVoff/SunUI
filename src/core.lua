a9a79af269fb1ef8166716aa95d39415ec4bb466
AGTVoff <aaronnallet2104@gmail.com>
2025-10-30 18:56:38 +0100

    Update ScreenGui parent to PlayerGui
    Set the parent of ScreenGui to the PlayerGui of the LocalPlayer.

--- src/core.lua
+++ src/core.lua
@@ -2,8 +2,9 @@
 local Core = {}
 
 function Core:Init(name)
-    local ScreenGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
+    local ScreenGui = Instance.new("ScreenGui")
     ScreenGui.Name = name or "SunUI"
+    ScreenGui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
     ScreenGui.ResetOnSpawn = false
     return ScreenGui
 end
