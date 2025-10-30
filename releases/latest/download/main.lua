923619ba36e310df905ffa795d8c65df39ac6831
AGTVoff <aaronnallet2104@gmail.com>
2025-10-30 19:04:55 +0100

    Update main.lua

--- releases/latest/download/main.lua
+++ releases/latest/download/main.lua
@@ -1,1 +1,12 @@
+-- main.lua
+local SunUI = {}
 
+local base = "https://raw.githubusercontent.com/AGTVoff/SunUI/main/src/"
+
+SunUI.Theme = loadstring(game:HttpGet(base .. "theme.lua"))()
+SunUI.Utils = loadstring(game:HttpGet(base .. "utils.lua"))()
+SunUI.Core = loadstring(game:HttpGet(base .. "core.lua"))()
+SunUI.Components = loadstring(game:HttpGet(base .. "components.lua"))()
+SunUI.Window = loadstring(game:HttpGet(base .. "window.lua"))(SunUI)
+
+return SunUI
